2b84c95417fc26127966d673aadb059ad236bd18
Hasan <bdhasan009@gmail.com>
2023-01-16 10:51:30 +0600

    8.1 Flutter icon

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-            child: TextButton(onPressed: (){
-              print("You pressed me");
-            }, child: Text("Click Me"),),
-
-            // Container(
-            // child: ElevatedButton(onPressed: (){
-            //   print("You pressed me");
-            // }, child: Text("Click Me"),),
-            //
-
+          child: Icon(
+            Icons.access_alarm,
+            color: Colors.pink,
+             size: 74.0,
           ),
         ),
       ),
