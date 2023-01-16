7764f34ea246d9ff6ec8f4a51b9da413ad1353b4
Hasan <bdhasan009@gmail.com>
2023-01-16 10:30:32 +0600

    7.1 Flutter Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,32 @@
 
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(myApp());
+  runApp(MyApp());
 }
-class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                height: 300.0,
-                width: double.infinity,
-                color: Colors.purple,
-              ),
-            ), Expanded(
-              flex: 1,
-              child: Container(
-                height: 300.0,
-                width: double.infinity,
-                color: Colors.green,
-              ),
-            ),
-          ],
+        body: Center(
+          child: Container(
+            child: TextButton(onPressed: (){
+              print("You pressed me");
+            }, child: Text("Click Me"),),
+
+            // Container(
+            // child: ElevatedButton(onPressed: (){
+            //   print("You pressed me");
+            // }, child: Text("Click Me"),),
+            //
+
+          ),
         ),
       ),
     );
