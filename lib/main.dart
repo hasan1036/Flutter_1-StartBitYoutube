e82d3c1ffd0f4d1700183dfdbcd090241883ccbc
Hasan <bdhasan009@gmail.com>
2023-01-16 14:40:44 +0600

    10.1 Flutter Network Image, elevation

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
-
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(MyApps());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApps extends StatelessWidget {
+  const MyApps({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: SizedBox(
-            height: 50.0,
-            width: 100.0,
-            child: ElevatedButton(onPressed: (){}, child: Row(
-              children: [
-                Icon(Icons.search),
-                Text("Search"),
-              ],
-            )),
+        appBar: AppBar(
+          title: Text("Cat"),
+        ),
+        body: Container(
+          color: Colors.red,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.4),
+              child: Card(
+                elevation: 50.0,
+                child: Image.network(
+                    "https://d2zp5xs5cp8zlg.cloudfront.net/image-31454-800.jpg"),
+              ),
+            ),
           ),
         ),
       ),
